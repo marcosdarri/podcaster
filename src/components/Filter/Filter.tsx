107427efb2aab39a808f7a4467d7c0ee7b94b050
marcosdarri <marcosdarri@hotmail.com>
2023-06-03 01:03:28 -0300

    Feat: Adds Filter component.

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Col, Row, Input } from "reactstrap";
+
+import style from "./Filter.module.scss"
+
+const Filter = ({setFilterText, total}) => {
+  return (
+    <Row className={style.filterRow}>
+        <Col className={style.col}>
+          <div className={style.total}>{total}</div>
+          <Input
+            className={style.filter}
+            onChange={(e) => setFilterText(e.target.value)}
+            type="search"
+            placeholder="Filter podcasts..."
+          />
+        </Col>
+      </Row>
+  )
+}
+
+export default Filter
