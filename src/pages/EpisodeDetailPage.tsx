caf44b51d4bd72f437ffa9c7abdb232921aad3c9
marcosdarri <marcosdarri@hotmail.com>
2023-06-03 22:29:45 -0300

    Feat: Adds new EpisodeDetail page.

---
+++ src/pages/EpisodeDetailPage.tsx
@@ -0,0 +1,10 @@
+
+import EpisodeDetail from "../components/EpisodeDetail/EpisodeDetail"
+
+const EpisodeDetailPage = () => {
+  return (
+    <EpisodeDetail/>
+  )
+}
+
+export default EpisodeDetailPage
