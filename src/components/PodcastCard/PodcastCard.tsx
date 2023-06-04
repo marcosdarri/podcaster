ab7a29a510690034f4b8588382e9189486563045
marcosdarri <marcosdarri@hotmail.com>
2023-06-03 22:28:16 -0300

    Feat: Adds PodcastDetail.

--- src/components/PodcastCard/PodcastCard.tsx
+++ src/components/PodcastCard/PodcastCard.tsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  UncontrolledTooltip,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
 import style from "./PodcastCard.module.scss";
 
-const PodcastCard = ({ img, title, author }) => {
+const PodcastCard = ({ id, img, title, author }) => {
   return (
-    <Link to={"/podcast"}>
+    <Link to={`/podcast/${id}`}>
       <Card className={style.card}>
-        <img alt="Sample" src={img} className={style.img} />
+        <img alt="PodcastCard" src={img} className={style.img} />
         <CardBody className={style.body}>
-          <CardTitle tag="h5">{title}</CardTitle>
+          <UncontrolledTooltip placement="right" target={`tooltip${id}`} style={{textAlign: "left"}}>
+            <b>Complete title:</b><br></br>
+            {title}
+          </UncontrolledTooltip>
+          <CardTitle id={`tooltip${id}`} tag="h5">
+            {title}
+          </CardTitle>
           <CardSubtitle className="mb-2 text-muted" tag="h6">
             Author: {author}
           </CardSubtitle>
