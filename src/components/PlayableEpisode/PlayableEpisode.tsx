e2189dcf11f0dc92516464d0c8e35dd96ad9994b
marcosdarri <marcosdarri@hotmail.com>
2023-06-04 21:32:50 -0300

    Feat: Adds PlayableEpisode.

---
+++ src/components/PlayableEpisode/PlayableEpisode.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Col } from "reactstrap";
+
+import style from "./PlayableEpisode.module.scss";
+
+const PlayableEpisode = ({ playableInfo }) => {
+  console.log(playableInfo);
+  return (
+    <Col className={style.col}>
+      <h2>{playableInfo.trackName}</h2>
+
+      <div className={style.description}
+        dangerouslySetInnerHTML={{
+          __html: playableInfo.description,
+        }}
+      />
+      <audio controls className={style.audio}>
+        <source src={playableInfo.episodeUrl} type="audio/ogg" />
+        <source src={playableInfo.episodeUrl} type="audio/mpeg" />
+        <source src={playableInfo.episodeUrl} type="audio/mp3" />
+        Your browser does not support the audio element.
+      </audio>
+    </Col>
+  );
+};
+
+export default PlayableEpisode;
