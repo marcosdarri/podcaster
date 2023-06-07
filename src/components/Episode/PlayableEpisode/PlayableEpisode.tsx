dfe0843f3ee234351cdd345b7518c7727420a1e1
marcosdarri <marcosdarri@hotmail.com>
2023-06-07 03:09:15 -0300

    Feat: Adds convertURLlinks function.

--- src/components/Episode/PlayableEpisode/PlayableEpisode.tsx
+++ src/components/Episode/PlayableEpisode/PlayableEpisode.tsx
@@ -5,13 +5,25 @@ import style from "./PlayableEpisode.module.scss";
 
 const PlayableEpisode = ({ episode }) => {
 
+  const convertURLlinks = texto => {
+    const regex = /(https?:\/\/[^\s]+)/g;
+  
+    const resultado = texto.replace(regex, url => {
+      return `<a href="${url}" target="_blank">${url}</a>`;
+    });
+  
+    return resultado;
+  };
+  
+
   return (
     <Col className={style.col}>
       <h2>{episode.trackName}</h2>
 
-      <div className={style.description}
+      <div
+        className={style.description}
         dangerouslySetInnerHTML={{
-          __html: episode.description,
+          __html: convertURLlinks(episode.description),
         }}
       />
       <audio controls className={style.audio}>
