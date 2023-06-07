fecff4b0726484c33f0e8bca254da0f5a8fccc84
marcosdarri <marcosdarri@hotmail.com>
2023-06-07 03:08:38 -0300

    Refactpr: Changes PodcastInfo data.

--- src/components/Podcasts/PodcastInfo/PodcastInfo.tsx
+++ src/components/Podcasts/PodcastInfo/PodcastInfo.tsx
@@ -2,15 +2,15 @@ import style from "./PodcastInfo.module.scss";
 import { Row } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const PodcastInfo = ({ id, podcast, episodeDetail }) => {
+const PodcastInfo = ({ id, podcast,podcastInfo, episodeDetail }) => {
   return (
     <Row className={style.row}>
       {episodeDetail ? (
         <Link to={`/podcast/${id}`} className={style.link}>
-          <img alt="PodcastInfo" src={podcast.img} className={style.img} />
+          <img alt="PodcastInfo" src={podcastInfo.artworkUrl600} className={style.img} />
         </Link>
       ) : (
-        <img alt="PodcastInfo" src={podcast.img} className={style.img} />
+        <img alt="PodcastInfo" src={podcastInfo.artworkUrl600} className={style.img} />
       )}
       <div className={style.title}>
         {episodeDetail ? (
@@ -18,12 +18,12 @@ const PodcastInfo = ({ id, podcast, episodeDetail }) => {
             to={`/podcast/${id}`}
             style={{ textDecoration: "none", color: "black" }}
           >
-            <h5>{podcast.title}</h5>
+            <h5>{podcastInfo.collectionName}</h5>
             <p>by {podcast.author}</p>
           </Link>
         ) : (
           <>
-            <h5>{podcast.title}</h5>
+            <h5>{podcastInfo.collectionName}</h5>
             <p>by {podcast.author}</p>
           </>
         )}
